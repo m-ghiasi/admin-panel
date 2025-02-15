db4d90343749e58395c3e6b0a45bdec89eb93fc0
m-ghiasi <mahdiye71gh@gmail.com>
2025-02-15 09:47:37 +0330

    edit aside style

--- src/components/Aside/index.tsx
+++ src/components/Aside/index.tsx
@@ -1,43 +1,86 @@
 import { NavLink } from "react-router";
 import { RxDashboard } from "react-icons/rx";
-import { MdOutlineAddToPhotos } from "react-icons/md";
+
 import { MdOutlineManageAccounts } from "react-icons/md";
 import { MdPermIdentity } from "react-icons/md";
 import { IoSettingsOutline } from "react-icons/io5";
 import { RiBillLine } from "react-icons/ri";
 import { SiFitbit } from "react-icons/si";
+import { MdProductionQuantityLimits } from "react-icons/md";
 
 export default function Aside() {
   return (
     <div className="w-full h-full relative ">
-      <div className="group w-[10%] h-full bg-purple-600   hover:w-[30%] transition-all duration-300  flex flex-col gap-5 pl-10 pt-10">
-        <NavLink className="pb-10 flex " to="home">
-          <SiFitbit color="white" size={50} />
-          <span className="opacity-0 group-hover:opacity-100 transition-opacity duration-300  text-white font-bold">Fitilo </span>
+      <div className="group w-[5%] h-full bg-purple-600   hover:w-[18%] transition-all duration-300  flex flex-col gap-7 pl-[1%] pt-10">
+        <NavLink className="pb-10 flex gap-3 items-center " to="home">
+          <SiFitbit
+            className="min-w-[40px] min-h-[40px]"
+            color="white"
+            size={30}
+          />
+          <span className="overflow-hidden w-0 group-hover:w-auto opacity-0 group-hover:opacity-100 group-hover:font-bold text-2xl transition-opacity duration-300  text-white ">
+            Fitilo
+          </span>
         </NavLink>
-        <NavLink to="/home/Services">
-          <RxDashboard size={30} color="white" />
-          <span className="opacity-0 group-hover:opacity-100 transition-opacity duration-300  text-white font-bold">Dashboard</span>
+        <NavLink className="flex gap-3 items-center" to="/home/Services">
+          <RxDashboard
+            className="min-w-[30px] min-h-[30px]"
+            size={30}
+            color="white"
+          />
+          <span className="overflow-hidden w-0 group-hover:w-auto opacity-0 group-hover:opacity-100 transition-opacity duration-300  text-white ">
+            Dashboard
+          </span>
         </NavLink>
-        <NavLink to="/home/Services">
-          <MdOutlineAddToPhotos size={30} color="white" />
-          <span className="opacity-0 group-hover:opacity-100 transition-opacity duration-300  text-white font-bold">Dashboard</span>
+        <NavLink className="flex gap-3 items-center" to="/home/Services">
+          <MdProductionQuantityLimits
+            className="min-w-[30px] min-h-[30px]"
+            size={30}
+            color="white"
+          />
+          <span className="overflow-hidden w-0 group-hover:w-auto opacity-0 group-hover:opacity-100 transition-opacity duration-300  text-white ">
+            Services
+          </span>
         </NavLink>
-        <NavLink to="/home/Services">
-          <MdOutlineManageAccounts size={30} color="white" />
-          <span className="opacity-0 group-hover:opacity-100 transition-opacity duration-300  text-white font-bold">Dashboard</span>
+        <NavLink className="flex gap-3 items-center" to="/home/Services">
+          <MdOutlineManageAccounts
+            className="min-w-[30px] min-h-[30px]"
+            size={30}
+            color="white"
+          />
+          <span className="overflow-hidden w-0 group-hover:w-auto opacity-0 group-hover:opacity-100 transition-opacity duration-300  text-white ">
+          Customer
+          </span>
         </NavLink>
-        <NavLink to="/home/Services">
-          <MdPermIdentity size={30} color="white" />
-          <span className="opacity-0 group-hover:opacity-100 transition-opacity duration-300  text-white font-bold">Dashboard</span>
+        <NavLink className="flex gap-3 items-center" to="/home/Services">
+          <MdPermIdentity
+            className="min-w-[30px] min-h-[30px]"
+            size={30}
+            color="white"
+          />
+          <span className="overflow-hidden w-0 group-hover:w-auto opacity-0 group-hover:opacity-100 transition-opacity duration-300  text-white ">
+            Dashboard
+          </span>
         </NavLink>
-        <NavLink to="/home/Services">
-          <IoSettingsOutline size={30} color="white" />
-          <span className="opacity-0 group-hover:opacity-100 transition-opacity duration-300  text-white font-bold">Dashboard</span>
+        <NavLink className="flex gap-3 items-center" to="/home/Services">
+          <IoSettingsOutline
+            className="min-w-[30px] min-h-[30px]"
+            size={30}
+            color="white"
+          />
+          <span className="overflow-hidden w-0 group-hover:w-auto opacity-0 group-hover:opacity-100 transition-opacity duration-300  text-white ">
+            Setting
+          </span>
         </NavLink>
-        <NavLink to="/home/Services">
-          <RiBillLine size={30} color="white" />
-          <span className="opacity-0 group-hover:opacity-100 transition-opacity duration-300  text-white font-bold">Dashboard</span>
+        <NavLink className="flex gap-3 items-center" to="/home/Services">
+          <RiBillLine
+            className="min-w-[30px] min-h-[30px]"
+            size={30}
+            color="white"
+          />
+          <span className=" overflow-hidden w-0 group-hover:w-auto opacity-0 group-hover:opacity-100 transition-opacity duration-300  text-white ">
+            Dashboard
+          </span>
         </NavLink>
       </div>
     </div>
