192ef843ece07c7debc4bbe86d4be27339441fd4
m-ghiasi <mahdiye71gh@gmail.com>
2025-02-19 11:13:54 +0330

    create steps compponents

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,39 +1,65 @@
 import { useState } from "react";
 import Button from "../../components/Button";
+import Step1 from "./Forms/step1";
+import Step2 from "./Forms/Step2";
+import Step3 from "./Forms/Step3";
+import Step4 from "./Forms/Step4";
 
 export default function Profile() {
-    const [step, setStep]= useState<number>(1)
+  const [step, setStep] = useState<number>(1);
 
-    const stepLength=[1,2,3,4]
-    const handelStep = (e:React.FormEvent<HTMLFormElement>)=>{
-        e.preventDefault()
-       if(step < 4) {setStep(step+1)}
-       console.log(step)
-
+  const stepLength = [1, 2, 3, 4];
+  const handelStep = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+    if (step < 4) {
+      setStep(step + 1);
     }
-
+    console.log(step);
+  };
 
   return (
-    <div className="bg-purple-700 w-full h-full flex items-center justify-center
-     ">
-      <form className=" bg-white w-88 p-4 rounded-2xl">
+    <div
+      className="bg-purple-700 w-full h-full flex items-center justify-center
+     "
+    >
+      <form className=" bg-white w-96 p-4 rounded-2xl">
         <header>
           <h1 className="font-bold text-2xl">Information</h1>
         </header>
-        <hr className="text-gray-200 my-3"/>
+        <hr className="text-gray-200 my-3" />
         {/* step */}
         <div className="flex justify-evenly gap-2">
-            {stepLength.map((st)=>(
-                <div key={st} className={` w-8 rounded-full flex items-center justify-center  h-8 font-bold ${st<= step ? "bg-purple-500 text-white":"bg-white text-purple-500"}`}>{st}</div>
-            ))}
+          {stepLength.map((st) => (
+            <div
+              key={st}
+              className={` w-8 rounded-full flex items-center justify-center  h-8  ${
+                st <= step
+                  ? "bg-purple-500 text-white"
+                  : "bg-white text-purple-500"
+              }`}
+            >
+              {st}
+            </div>
+          ))}
         </div>
 
         {/* content */}
-        <div></div>
+        <div  className="my-3 min-h-50">
+            {step===1? <Step1/>:
+            step===2? <Step2/>:
+            step===3 ? <Step3/>:
+            <Step4/>}
+            
+        </div>
 
         {/* button */}
-        <Button onClick={handelStep} label="NEXT" className="bg-purple-500 text-white font-bold rounded-2xl p-3" />
-
+        <div className="flex justify-center">
+        <Button
+          onClick={handelStep}
+          label={step===4?"FINISH": "NEXT"}
+          className="bg-purple-500 text-white  rounded-2xl p-3 px-6"
+        />
+        </div>
       </form>
     </div>
   );
