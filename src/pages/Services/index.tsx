488463d0e31abca56303c90df3c76462972eadeb
m-ghiasi <mahdiye71gh@gmail.com>
2025-02-25 14:10:08 +0330

    services style

--- src/pages/Services/index.tsx
+++ src/pages/Services/index.tsx
@@ -6,57 +6,53 @@ import Button from "../../components/Button";
 export default function Services() {
   const [services, setServices] = useState<any>([]);
 
-  const [selectTab, setSelectedTab]= useState<string | null>("all")
-  const handelServices= ()=>{
-    setSelectedTab("all")
-  }
-  const handelMaster= ()=>{
-    setSelectedTab("master")
-  }
-  const handelBachelor= ()=>{
-    setSelectedTab("bachelor")
-  }
+  const [selectTab, setSelectedTab] = useState<string | null>("all");
+  const handelServices = () => {
+    setSelectedTab("all");
+  };
+  const handelMaster = () => {
+    setSelectedTab("master");
+  };
+  const handelBachelor = () => {
+    setSelectedTab("bachelor");
+  };
 
-
-  const filterServices = services.filter((service:any)=> {
-    if(selectTab==="master") return service.id < 5;;
-    if(selectTab === "bachelor") return service.id >= 8 ;
+  const filterServices = services.filter((service: any) => {
+    if (selectTab === "master") return service.id < 5;
+    if (selectTab === "bachelor") return service.id >= 8;
     return true;
-  })
+  });
 
   return (
-    <div className="overflow-y-scroll px-10 mt-10 mb-10">
-      <div className="flex gap-1 px-20">
-        <Button
-          label="All State"
-          className="font-bold rounded-t-2xl bg-purple-600 px-10 py-3 text-white"
-          onClick={handelServices}
-        />
-        <Button
-          label="Master"
-          className="font-bold rounded-t-2xl bg-purple-600 px-10 py-3 text-white"
-          onClick={handelMaster}
-        />
-        <Button
-          label="Bachelor"
-          className="font-bold rounded-t-2xl bg-purple-600 px-10 py-3 text-white"
-          onClick={handelBachelor}
-        />
-      </div>
+    <div className=" overflow-auto px-10 mt-10 mb-10">
       <GetDataFetch
         url="https://jsonplaceholder.typicode.com/users"
         handleFetchedData={setServices}
       />
+      <div className="flex gap-1 px-20">
+        <Button
+          label="All State"
+          className={`font-bold rounded-t-2xl px-10 py-3 ${selectTab==="all"? "bg-white text-purple-600": "bg-purple-600 text-white" }` }
+          onClick={handelServices}
+        />
+        <Button
+          label="Master"
+          className={`font-bold rounded-t-2xl px-10 py-3 ${selectTab==="master"? "bg-white text-purple-600": "bg-purple-600 text-white" }` }
+          onClick={handelMaster}
+        />
+        <Button
+          label="Bachelor"
+          className={`font-bold rounded-t-2xl px-10 py-3 ${selectTab==="bachelor"? "bg-white text-purple-600": "bg-purple-600 text-white" }` }
+          onClick={handelBachelor}
+        />
+      </div>
 
-      <div className="flex flex-wrap gap-5 ml-20 p-5 shadow-xl shadow-b-xl pb-10 shadow-purple-300  rounded-2xl  md:w-[85%] ">
-        {filterServices.lenghth === 0 ? 
-        (
-            <div>is loading...</div>
+      <div className="flex flex-wrap gap-5 ml-20 p-5 shadow-xl shadow-b-xl pb-10 shadow-purple-300  rounded-2xl min-w-[85%] ">
+        {filterServices.lenghth === 0 ? (
+          <div>is loading...</div>
         ) : (
-              
-                filterServices.map((service: any, index: number)=>(
-
-              <div key={index} className="justify-between">
+          filterServices.map((service: any, index: number) => (
+            <div key={index} className="justify-between">
               <TemplateCard
                 id={service.id}
                 name={service.name}
@@ -67,10 +63,8 @@ export default function Services() {
                 website={service.website}
               />
             </div>
-
-                ))
-           
-        ) }
+          ))
+        )}
       </div>
     </div>
   );
