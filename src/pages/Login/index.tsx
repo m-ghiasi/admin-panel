ebc7d736b9aa5dbc2cac064b9a26f55d191d4e00
m-ghiasi <mahdiye71gh@gmail.com>
2025-03-01 11:34:20 +0330

    responsive-login edit

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -27,15 +27,15 @@ export default function Login() {
 
   return (
     <div className="flex justify-center items-center h-screen w-screen ">
-      <div className=" w-[50%] h-[70%] md:border md:border-gray-300 rounded-2xl flex flex-col items-center justify-center">
+      <div className="p-2 w-[50%] h-[70%] md:border md:border-gray-300 rounded-2xl flex flex-col items-center justify-center gap-2">
         <SiFitbit size={50} color="purple" />
         <h1 className="font-bold mt-3">Login To User Panel</h1>
-        <br />
+        
         <TextField label="User Name" id="name" className="placeholder:Username" type="text" onChange={(e)=> setUsername(e.target.value)}/>
         <TextField label="Password" id="name" className="placeholder:Username" type="password" onChange={(e)=>  setPassword(e.target.value)}/>
-        <br />
+        
         <NavLink className= "text-blue-600" to={"/ForgetPassword"}>Forget Password</NavLink>
-        <br />
+        
         <Button onClick={hadelLogin} label="Login" className="bg-purple-800 text-white font-bold w-50 p-2 rounded-2xl" />
         
 
